fd7cafa6d7d96674146401a838e771f859f56f9e
Michael <michaelmathiasbehr@hotmail.com>
2026-03-04 15:44:14 +0100

    Add project files.

---
+++ DayZero/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace DayZero
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ DayZero/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DayZero
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
